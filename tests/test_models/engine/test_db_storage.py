b90d16737ba81100316c84dc7f09b3f8af89198a
Nosa <nosakhareaghedo42@gmail.com>
2022-12-10 21:32:54 +0000

    wrote some tests

--- tests/test_models/engine/test_db_storage.py
+++ tests/test_models/engine/test_db_storage.py
@@ -6,6 +6,14 @@ from models.vote import Vote
 from models.user import User
 from models import storage
 
+"""creation of needed objects"""
+kwargs = {'name': "james", 'email':"james@gmail.com", 'password': "jamespwd"}
+user = User(**kwargs)
 
+kwargs = {'title': 'testing', 'content': 'contents of test', 'user_id': user.id}
+post = Post(**kwargs)
+
+kwargs = {'content': 'contents of test'}
+comment = Comment(**kwargs)
 """ unittests for database storage """
 
