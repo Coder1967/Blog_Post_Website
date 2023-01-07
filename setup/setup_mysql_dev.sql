98b94a4244f4c14facbc189ba4c3f30ed733b8fa
Nosa <nosakhareaghedo42@gmail.com>
2023-01-07 19:39:35 +0000

    reverting back to the original setup

--- setup/setup_mysql_dev.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS blog_db;
-CREATE USER IF NOT EXISTS 'dev'@'localhost' IDENTIFIED BY "$MYSQL_PWD";
-GRANT ALL PRIVILEGES ON `blog_db`.* TO 'dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'dev'@'localhost';
-FLUSH PRIVILEGES;
--- setup/start.sh
+++
@@ -1,3 +0,0 @@
-cat setup_mysql_dev.sql | mysql -hlocalhost -uroot
-gunicorn --bind 0.0.0.0:5000 api.v1.app:app --daemon
-gunicorn --bind 0.0.0.0:5001 front_end.index:app --daemon
