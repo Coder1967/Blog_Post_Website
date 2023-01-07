dd7f23d62d4898daf8b294137c3e5fdc2ddd8053
Nosa <nosakhareaghedo42@gmail.com>
2023-01-07 18:46:29 +0000

    adding files to help in deployment

--- setup/start.sh
+++ setup/start.sh
@@ -1,2 +1,2 @@
-tmux new-session -d 'gunicorn --bind 0.0.0.0:5000 api.v1.app:app'
-tmux new-session -d 'gunicorn --bind 0.0.0.0:5001 front_end.index:app'
+gunicorn --bind 0.0.0.0:5000 api.v1.app:app --daemon
+gunicorn --bind 0.0.0.0:5001 front_end.index:app --daemon
